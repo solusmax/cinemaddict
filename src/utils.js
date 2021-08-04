35828883d101cf393ea4284e217a696858597091
solusmax <solusmaxim@gmail.com>
2021-08-04 11:01:13 +0300

    Вынес пару констант в отдельный файл

--- src/utils.js
+++ src/utils.js
@@ -2,26 +2,14 @@ import dayjs from 'dayjs';
 import dayjsDuration from 'dayjs/plugin/duration';
 import dayjsMinMax from 'dayjs/plugin/minMax';
 import dayjsRelativeTime from 'dayjs/plugin/relativeTime';
+import {
+  MINUTES_IN_ONE_DAY,
+  RANDOM_SENTENCES
+} from './constants.js';
 
 dayjs.extend(dayjsDuration);
 dayjs.extend(dayjsMinMax);
 dayjs.extend(dayjsRelativeTime);
-
-const RANDOM_SENTENCES = [
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  'Cras aliquet varius magna, non porta ligula feugiat eget.',
-  'Fusce tristique felis at fermentum pharetra.',
-  'Aliquam id orci ut lectus varius viverra.',
-  'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.',
-  'Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.',
-  'Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.',
-  'Sed sed nisi sed augue convallis suscipit in sed felis.',
-  'Aliquam erat volutpat.',
-  'Nunc fermentum tortor ac porta dapibus.',
-  'In rutrum ac purus sit amet tempus.',
-];
-
-const MINUTES_IN_ONE_DAY = 1440;
 
 export const getRandomInteger = (min = 0, max = 1) => {
   min = Math.ceil(min);
