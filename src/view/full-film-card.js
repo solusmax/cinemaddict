a0ab07858076da356961a43449c6dc263fa822b0
solusmax <solusmaxim@gmail.com>
2021-09-10 01:01:09 +0300

    Добавил he и обработку комментария при выводе

--- src/view/full-film-card.js
+++ src/view/full-film-card.js
@@ -1,4 +1,5 @@
 import AbstractFilmCardView from './abstract-film-card';
+import he from 'he';
 import {
   addPluralEnding,
   getFormattedDuration,
@@ -33,7 +34,7 @@ const createCommentTemplate = (comments, id) => {
         <img src="./images/emoji/${emoji}.png" width="55" height="55" alt="emoji-${emoji}">
       </span>
       <div>
-        <p class="film-details__comment-text">${text}</p>
+        <p class="film-details__comment-text">${he.encode(text)}</p>
         <p class="film-details__comment-info">
           <span class="film-details__comment-author">${author}</span>
           <span class="film-details__comment-day">${humanizeCommentDate(date)}</span>
